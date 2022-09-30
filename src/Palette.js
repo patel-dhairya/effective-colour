4a73d5d8a51563dfe346e3b1f3538e0330c29987
patel-dhairya <p5d7@proton.me>
2022-09-30 23:43:43 +0530

    Added color collection
    Added color collection file "colorCollection.js" which contains colors name and hex codes

---
+++ src/Palette.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+export default class Palette extends Component {
+    render(){
+        return(
+            <div className='Palette'>
+                {/* Navbar */}
+                <div className='Palette-colors'> 
+                {/* Color boxes */}
+                </div>
+                {/* Footer */ }
+            </div>
+        )
+    }
+}
+
+// export default Palette;
